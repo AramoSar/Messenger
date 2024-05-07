31414e96df203e8919a62fdc894d9ed00d604df9
Aram <aramsaribekyan1@gmail.com>
2024-05-07 09:29:58 +0400

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Aram on 07.05.24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
