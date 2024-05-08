014fd2bfd0aeaa3a49b7c6273a0cf9f41aeaeca1
Aram <aramsaribekyan1@gmail.com>
2024-05-08 10:39:30 +0400

    First commit

---
+++ Messenger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,122 @@
+//
+//  LoginView.swift
+//  Messenger
+//
+//  Created by Aram on 07.05.24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                // logo image
+                Image(.messengerLogo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .padding()
+                
+                // text fields
+                VStack {
+                    TextField("Enter your email..", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                     
+                    SecureField("Enter your password..", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                }
+                .padding()
+                
+                // forgot password
+                
+                Button {
+                    print("Forgot password")
+                } label: {
+                    Text("Forgot password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                
+                // Login button
+                Button{
+                    print("Handle Login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(10)
+                }
+                .padding(.vertical)
+                
+                // facebook login
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width) / 2 - 40, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width) / 2 - 40, height: 0.5)
+                }
+                .foregroundStyle(.gray)
+                
+                HStack {
+                    Image(.facebookLogo)
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color(.systemBlue))
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                // sign up link
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign up View")
+                } label: {
+                    HStack {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical)
+                
+                
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
